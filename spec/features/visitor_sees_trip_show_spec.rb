6a62b2624f55fd00b4aa5001d576935e7398da8e
Angela Duran <duranangela19@gmail.com>
2018-07-27 10:32:12 -0600

    Add feature test for trails on trips show page

--- spec/features/visitor_sees_trip_show_spec.rb
+++ spec/features/visitor_sees_trip_show_spec.rb
@@ -3,12 +3,20 @@ require 'rails_helper'
 describe 'visitor sees trips show page' do
   it 'sees show page for individual trip' do
     trip1 = Trip.create(name: 'hike1', start_date: "8/20/18", end_date: "8/20/18")
-    trip2 = Trip.create(name: 'hike2', start_date: "8/21/18", end_date: "8/21/18")
+    trail1 = trip1.create(length: 4, name: 'trail1', address: 'mountains')
+    trail2 = trip1.create(length: 6, name: 'trail2', address: 'river')
 
     visit trips_path
 
     click_on trip1.name
 
     expect(current_path).to eq(trip_path(trip1))
+    expect(page).to have_content(trail1.name)
+    expect(page).to have_content(trail1.address)
+    expect(page).to have_content(trail1.length)
+    expect(page).to have_content(trail2.name)
+    expect(page).to have_content(trail2.address)
+    expect(page).to have_content(trail2.length)
+
   end
 end
