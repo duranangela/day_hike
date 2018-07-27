62fd405ba72cb534dc01327973aed14b2ae8981f
Angela Duran <duranangela19@gmail.com>
2018-07-27 10:51:37 -0600

    Add method total_lenght to trip

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -2,5 +2,5 @@ class Trail < ApplicationRecord
 
   has_many :trail_trips
   has_many :trips, through: :trail_trips
-  
+
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,8 @@ class Trip < ApplicationRecord
   has_many :trail_trips
   has_many :trails, through: :trail_trips
 
+  def total_length
+    trails.sum(:length)
+  end
+
 end
